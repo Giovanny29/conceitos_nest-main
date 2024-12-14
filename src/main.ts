1607c010491a155803e3858937b717b6d3319b5c
Giovanny <giovannyaraujo29@gmail.com>
2024-12-14 15:42:17 -0300

    Add uni tests and e2e tests

--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
-import { ValidationPipe } from '@nestjs/common';
-import { ParseIntIdPipe } from './common/pipes/parse-int-id.pipe';
+import appConfig from './app/config/app.config';
+import helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.useGlobalPipes(
-    new ValidationPipe({
-      whitelist: true, // remove chaves que não estão no DTO
-      forbidNonWhitelisted: true, // levantar erro quando a chave não existir
-      transform: false, // tenta transformar os tipos de dados de param e dtos
-    }),
-    new ParseIntIdPipe(),
-  );
+  appConfig(app);
 
-  await app.listen(3000);
+  if (process.env.NODE_ENV === 'production') {
+    // helmet -> cabeçalhos de segurança no protocolo HTTP
+    app.use(helmet());
+
+    // cors -> permitir que outro domínio faça requests na sua aplicação
+    app.enableCors({
+      origin: 'https://meuapp.com.br',
+    });
+  }
+
+  await app.listen(process.env.APP_PORT);
 }
 bootstrap();
